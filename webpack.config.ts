85cb260b07ef9a6f451eab17108c2b368f4b31c5
Hogonov <43898789+Hogonov@users.noreply.github.com>
2023-02-08 14:18:15 +0600

    feet: add environment variables

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,16 +1,19 @@
 import path from 'path';
 import webpack from 'webpack';
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPath} from "./config/build/types/config";
+import {BuildEnv, BuildPath} from "./config/build/types/config";
 
-const paths: BuildPath = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html')
-}
+export default (env: BuildEnv):webpack.Configuration => {
+    const paths: BuildPath = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html')
+    }
 
-const mode = 'development'
-const isDev = mode === 'development'
-const config: webpack.Configuration = buildWebpackConfig({mode, paths, isDev})
+    const mode = env.mode || 'development'
+    const port = env.port || 3000
 
-export default config;
+    const isDev = mode === 'development'
+
+    return buildWebpackConfig({mode, paths, isDev, port})
+};
