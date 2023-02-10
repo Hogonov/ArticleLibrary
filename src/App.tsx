e6eb69ffda6035172c63136bfdbc00b2fd34111c
Hogonov <43898789+Hogonov@users.noreply.github.com>
2023-02-10 16:45:38 +0600

    feet: add helper classNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
 import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
 import './styles/index.scss'
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 export enum Theme {
     LIGHT = 'light',
     DARK = 'dark'
 }
+
 const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Изменить тему, сейчас {theme}</button>
             <Link to={'/'}>Главная </Link>
             <Link to={'/about'}> О сайте</Link>
