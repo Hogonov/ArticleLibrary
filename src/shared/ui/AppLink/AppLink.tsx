83086969b0de74b0a4e1d4e88813ad8e6eebe14b
Hogonov <43898789+Hogonov@users.noreply.github.com>
2023-02-12 15:47:03 +0600

    feet: add navbar

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,35 @@
+import {FC} from 'react';
+import classes from './AppLink.module.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {Link, LinkProps} from "react-router-dom";
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    INVERTED = 'inverted'
+}
+
+interface AppLinkProps extends LinkProps {
+    className?: string,
+    theme?: AppLinkTheme
+
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+    const {
+        className,
+        children,
+        to,
+        theme = AppLinkTheme.PRIMARY,
+        ...otherProps
+    } = props
+
+    return (
+        <Link
+            to={to}
+            className={classNames(classes.AppLink, {}, [className, classes[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+};
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,25 @@
+import {FC} from 'react';
+import classes from './Navbar.module.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string,
+
+}
+
+export const Navbar: FC<NavbarProps> = (props) => {
+    const {className} = props
+    return (
+        <div className={classNames(classes.Navbar, {}, [className])}>
+            <div className={classes.links}>
+                <AppLink
+                    to={'/'}
+                    className={classes.mainLink}
+                    theme={AppLinkTheme.INVERTED}
+                >Главная</AppLink>
+                <AppLink to={'/about'} theme={AppLinkTheme.INVERTED}>О сайте</AppLink>
+            </div>
+        </div>
+    );
+};
