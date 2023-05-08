fb784acd4bcdc40c227a6a49aca74d48a1d84a0d
Hogonov <43898789+Hogonov@users.noreply.github.com>
2023-05-08 15:39:23 +0600

    fix lint error

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import classes from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,6 +11,7 @@ interface NavbarProps {
 
 export const Navbar: FC<NavbarProps> = (props) => {
     const { className } = props;
+    const { t } = useTranslation();
     return (
         <div className={classNames(classes.Navbar, {}, [className])}>
             <div className={classes.links}>
@@ -18,9 +20,9 @@ export const Navbar: FC<NavbarProps> = (props) => {
                     className={classes.mainLink}
                     theme={AppLinkTheme.INVERTED}
                 >
-                    Главная
+                    {t('Главная')}
                 </AppLink>
-                <AppLink to="/about" theme={AppLinkTheme.INVERTED}>О сайте</AppLink>
+                <AppLink to="/about" theme={AppLinkTheme.INVERTED}>{t('О сайте')}</AppLink>
             </div>
         </div>
     );
