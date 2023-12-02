ad82fdb72bdc7cc1ad84e3e70738d8d58203e782
Hogonov <43898789+Hogonov@users.noreply.github.com>
2023-12-02 18:03:35 +0600

    add strict mode, add profile card

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,3 +1,4 @@
 export { profileReducer, profileActions } from './model/slice/profileSlice';
 export { Profile, ProfileSchema } from './model/types/profile';
-export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
+export { ProfileCard } from './ui/ProfileCard/ProfileCard';
+export { fetchProfileData } from './model/services/fetchProfileData/fetchProfileData';
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { profileReducer } from 'entities/Profile';
+import { fetchProfileData, profileReducer, ProfileCard } from 'entities/Profile';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 const reducers: ReducersList = {
     profile: profileReducer,
 };
 
 const ProfilePage = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { t } = useTranslation('main');
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <div>
-                {t('Страница профиля')}
-            </div>
+            <ProfileCard />
         </DynamicModuleLoader>
     );
 };
