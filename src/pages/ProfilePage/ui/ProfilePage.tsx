0c44e60b2163724b63ddfacb411bea2a3af0a1b7
Hogonov <43898789+Hogonov@users.noreply.github.com>
2023-12-27 18:47:58 +0600

    add profile

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,8 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { fetchProfileData, profileReducer, ProfileCard } from 'entities/Profile';
+import {
+    fetchProfileData,
+    profileReducer,
+    ProfileCard,
+    getProfileError,
+    getProfileIsLoading,
+    profileActions,
+    getProfileReadonly,
+    getProfileForm,
+} from 'entities/Profile';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { ProfilePageHeader } from './ProfilePageHeader/ProfilePageHeader';
 
 const reducers: ReducersList = {
     profile: profileReducer,
@@ -12,14 +23,54 @@ const ProfilePage = () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { t } = useTranslation('main');
     const dispatch = useAppDispatch();
+    const data = useSelector(getProfileForm);
+    const isLoading = useSelector(getProfileIsLoading);
+    const error = useSelector(getProfileError);
+    const readonly = useSelector(getProfileReadonly);
 
     useEffect(() => {
         dispatch(fetchProfileData());
     }, [dispatch]);
 
+    const onChangeFirstname = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ first: value || '' }));
+    }, [dispatch]);
+
+    const onChangeLastname = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ lastname: value || '' }));
+    }, [dispatch]);
+
+    const onChangeCity = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ city: value || '' }));
+    }, [dispatch]);
+
+    const onChangeAge = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ age: Number(value || 0) }));
+    }, [dispatch]);
+
+    const onChangeUsername = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ username: value || '' }));
+    }, [dispatch]);
+
+    const onChangeAvatar = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ avatar: value || '' }));
+    }, [dispatch]);
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <ProfileCard />
+            <ProfilePageHeader />
+            <ProfileCard
+                data={data}
+                isLoading={isLoading}
+                error={error}
+                readonly={readonly}
+                onChangeFirstname={onChangeFirstname}
+                onChangeLastname={onChangeLastname}
+                onChangeAge={onChangeAge}
+                onChangeCity={onChangeCity}
+                onChangeUsername={onChangeUsername}
+                onChangeAvatar={onChangeAvatar}
+            />
         </DynamicModuleLoader>
     );
 };
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,17 +1,19 @@
+import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import React, {
     InputHTMLAttributes, memo, useEffect, useRef, useState,
 } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Input.module.scss';
 
-type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange' | 'readOnly'>
 
-interface InputProps extends HTMLInputProps{
-    className?: string,
-    value?: string;
+interface InputProps extends HTMLInputProps {
+    className?: string;
+    value?: string | number;
     onChange?: (value: string) => void;
-    autoFocus?: boolean;
+    autofocus?: boolean;
+    readonly?: boolean;
 }
+
 export const Input = memo((props: InputProps) => {
     const {
         className,
@@ -19,37 +21,46 @@ export const Input = memo((props: InputProps) => {
         onChange,
         type = 'text',
         placeholder,
-        autoFocus,
+        autofocus,
+        readonly,
         ...otherProps
     } = props;
-
     const ref = useRef<HTMLInputElement>(null);
     const [isFocused, setIsFocused] = useState(false);
     const [caretPosition, setCaretPosition] = useState(0);
 
-    const onBlur = () => {
-        setIsFocused(false);
-    };
-    const onFocus = () => {
-        setIsFocused(true);
-    };
-    const onSelect = (e: any) => {
-        setCaretPosition(e?.target?.selectionStart || 0);
-    };
+    const isCaretVisible = isFocused && !readonly;
+
+    useEffect(() => {
+        if (autofocus) {
+            setIsFocused(true);
+            ref.current?.focus();
+        }
+    }, [autofocus]);
+
     const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         onChange?.(e.target.value);
         setCaretPosition(e.target.value.length);
     };
 
-    useEffect(() => {
-        if (autoFocus) {
-            setIsFocused(true);
-            ref.current?.focus();
-        }
-    }, [autoFocus]);
+    const onBlur = () => {
+        setIsFocused(false);
+    };
+
+    const onFocus = () => {
+        setIsFocused(true);
+    };
+
+    const onSelect = (e: any) => {
+        setCaretPosition(e?.target?.selectionStart || 0);
+    };
+
+    const mods: Mods = {
+        [cls.readonly]: readonly,
+    };
 
     return (
-        <div className={classNames(cls.InputWrapper, {}, [className])}>
+        <div className={classNames(cls.InputWrapper, mods, [className])}>
             {placeholder && (
                 <div className={cls.placeholder}>
                     {`${placeholder}>`}
@@ -58,24 +69,23 @@ export const Input = memo((props: InputProps) => {
             <div className={cls.caretWrapper}>
                 <input
                     ref={ref}
-                    className={cls.input}
                     type={type}
                     value={value}
                     onChange={onChangeHandler}
+                    className={cls.input}
                     onFocus={onFocus}
                     onBlur={onBlur}
                     onSelect={onSelect}
+                    readOnly={readonly}
                     {...otherProps}
                 />
-                {isFocused && (
+                {isCaretVisible && (
                     <span
                         className={cls.caret}
                         style={{ left: `${caretPosition * 9}px` }}
                     />
                 )}
             </div>
-
         </div>
-
     );
 });
