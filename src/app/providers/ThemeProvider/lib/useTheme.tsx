b8dd55b7de0cce5da757c5750e1709216e7ec36a
Hogonov <43898789+Hogonov@users.noreply.github.com>
2023-12-29 13:55:27 +0600

    add new theme, add concurrently

--- src/app/providers/ThemeProvider/lib/useTheme.tsx
+++ src/app/providers/ThemeProvider/lib/useTheme.tsx
@@ -5,11 +5,18 @@ interface UseThemeResult {
     toggleTheme: () => void;
     theme: Theme
 }
+
+const ThemeStrategy: Record<Theme, Theme> = {
+    [Theme.DARK]: Theme.LIGHT,
+    [Theme.LIGHT]: Theme.PURPLE_DARK,
+    [Theme.PURPLE_DARK]: Theme.DARK,
+};
+
 export function useTheme(): UseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
 
     const toggleTheme = () => {
-        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        const newTheme = theme ? ThemeStrategy[theme] : Theme.LIGHT;
         setTheme?.(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
         document.body.className = newTheme;
