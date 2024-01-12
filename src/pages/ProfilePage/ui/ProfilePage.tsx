6745ea466d90367028829bfd135108ff383f3052
Hogonov <43898789+Hogonov@users.noreply.github.com>
2024-01-12 16:11:30 +0600

    Implement UI state handling and move Page component to widgets module
    New UI state handling has been implemented with the UI feature and its corresponding Redux state, actions, and selectors. Page component has been moved from shared/ui to widgets module. Additionally, 'useThrottle' hook has been created and integrated into Page component. This allows to save scroll position in Redux state when user navigates away from the page and restores it when user goes back.

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -19,7 +19,7 @@ import { Currency } from 'entities/Currency';
 import { Country } from 'entities/Country';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useParams } from 'react-router-dom';
-import { Page } from 'shared/ui/Page/Page';
+import { Page } from 'widgets/Page/Page';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ProfilePageHeader } from './ProfilePageHeader/ProfilePageHeader';
 
