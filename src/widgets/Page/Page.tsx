a3a2206b16d9ce64251ec5bbe839afec0bbcb8d7
Hogonov <43898789+Hogonov@users.noreply.github.com>
2024-04-20 15:32:42 +0500

    Add react-virtualized and optimize article listing
    This commit integrates react-virtualized package for efficient rendering of large article lists. The ArticleList component has been refactored to leverage react-virtualized's WindowScroller and List components for optimal scrolling performance. The package.json has been updated to include 'react-virtualized' and necessary types. Minor unnecessary imports removed and other small code adjustments have been made for clean-up.

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -1,13 +1,13 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import {
-    MutableRefObject, ReactNode, useRef, UIEvent,
+    memo, MutableRefObject, ReactNode, UIEvent, useRef,
 } from 'react';
 import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
-import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
-import { getUIScrollByPath, uiActions } from 'features/UI';
-import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { getUIScrollByPath, uiActions } from 'features/UI';
+import { useLocation } from 'react-router-dom';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useSelector } from 'react-redux';
 import { StateSchema } from 'app/providers/StoreProvider';
 import { useThrottle } from 'shared/lib/hooks/useThrottle/useThrottle';
 import cls from './Page.module.scss';
@@ -18,7 +18,9 @@ interface PageProps {
     onScrollEnd?: () => void;
 }
 
-export const Page = (props: PageProps) => {
+export const PAGE_ID = 'PAGE_ID';
+
+export const Page = memo((props: PageProps) => {
     const { className, children, onScrollEnd } = props;
     const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
     const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
@@ -50,9 +52,10 @@ export const Page = (props: PageProps) => {
             ref={wrapperRef}
             className={classNames(cls.Page, {}, [className])}
             onScroll={onScroll}
+            id={PAGE_ID}
         >
             {children}
             {onScrollEnd ? <div className={cls.trigger} ref={triggerRef} /> : null}
         </section>
     );
-};
+});
