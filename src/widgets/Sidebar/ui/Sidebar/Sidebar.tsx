5fdda8e774f8268904a1a7910cd3ef4a3808a3ef
Hogonov <43898789+Hogonov@users.noreply.github.com>
2024-05-07 21:27:12 +0500

    Enhance accessibility and improve UI components
    This commit makes several changes across multiple components aiming to enhance accessibility and improve readability. Modified FlexProps to extend from DivProps, ensuring compatibility with native HTML attributes. In Sidebar.tsx, the HTML tag for the component was changed from <menu> to <aside> and a 'role' attribute was added to VStack for better semantic meaning. Adjustments were also made to the Text component, introducing new text sizes and changing the element type based on the size. Furthermore, ESLint rules were updated to recognize the 'role' attribute and the CSS variables were expanded to support the new text sizes.

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -22,7 +22,7 @@ export const Sidebar = memo((props: SidebarProps) => {
         setCollapsed((prev) => !prev);
     };
     return (
-        <menu
+        <aside
             data-testid="sidebar"
             className={
                 classNames(
@@ -44,7 +44,7 @@ export const Sidebar = memo((props: SidebarProps) => {
             >
                 {collapsed ? '>' : '<'}
             </Button>
-            <VStack gap="8" className={cls.items}>
+            <VStack role="navigation" gap="8" className={cls.items}>
                 {sidebarItemsList.map((item) => (
                     <SidebarItem item={item} collapsed={collapsed} key={item.path} />
                 ))}
@@ -54,6 +54,6 @@ export const Sidebar = memo((props: SidebarProps) => {
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} short={collapsed} />
             </div>
-        </menu>
+        </aside>
     );
 });
