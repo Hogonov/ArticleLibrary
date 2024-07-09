18c501a09694a0da30c49a05c88df4ccf7a6e7ac
Hogonov <43898789+Hogonov@users.noreply.github.com>
2024-07-09 15:33:50 +0500

    Update import paths and refactor Drawer component
    The import paths in the preview.ts file for storybook have been updated to use absolute paths. The Drawer component has been refactored to remove the AnimationProvider from the NotificationButton component, and instead incorporated directly into the Drawer component itself. This makes the usage of the Drawer component self-contained and improves encapsulation.

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react';
-import { Theme } from 'app/providers/ThemeProvider';
-import ThemeDecorator from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '@/app/providers/ThemeProvider';
+import ThemeDecorator from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import StyleDecorator from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import RouteDecorator from '../../src/shared/config/storybook/RouteDecorator/RouteDecorator';
 import SuspenseDecorator from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
