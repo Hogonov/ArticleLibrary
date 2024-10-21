cfa68aa6593f3c75a390a7bba2f694c95c2559e9
Hogonov <43898789+Hogonov@users.noreply.github.com>
2024-10-21 09:25:20 +0500

    Deprecate old UI components
    Moved old UI components to a deprecated directory and marked them with @deprecated. These components are to be replaced with new ones from the redesigned folder. Also, added new SVG icons for use in the updated UI.

---
+++ src/shared/ui/deprecated/Loader/Loader.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+import './Loader.scss';
+
+interface LoaderProps {
+    className?: string;
+}
+
+/**
+ * Устарел, используем новые компоненты из папки redesigned
+ * @deprecated
+ */
+export const Loader: FC<LoaderProps> = (props) => {
+    const { className } = props;
+    return (
+        <div className={classNames('lds-roller', {}, [className])}>
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+        </div>
+    );
+};
