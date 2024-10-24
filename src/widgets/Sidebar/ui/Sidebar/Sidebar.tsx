97ce03d4e37a2b11d96e355969b620f8b5b4ef23
Hogonov <43898789+Hogonov@users.noreply.github.com>
2024-10-24 09:13:29 +0500

    Update and redesign Sidebar and shared UI components
    Introduced redesigned versions of shared UI components such as Button, Icon, AppLink, and Sidebar. Deprecated corresponding old components, making adjustments to support feature toggling for a seamless transition. Enhanced the Sidebar's UX with collapsible functionality and modernized icons.

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,13 +1,15 @@
-import React, { memo, useState } from 'react';
+import { memo, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { LangSwitcher } from '@/features/LangSwitcher';
 import { ThemeSwitcher } from '@/features/ThemeSwitcher';
+import ArrowIcon from '@/shared/assets/icons/arrow-bottom.svg';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { ToggleFeatures } from '@/shared/lib/features';
-import { AppLogo } from '@/shared/ui/AppLogo';
-import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button';
-import { VStack } from '@/shared/ui/Stack';
+import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/deprecated/Button';
+import { VStack } from '@/shared/ui/deprecated/Stack';
+import { AppLogo } from '@/shared/ui/redesigned/AppLogo';
+import { Icon } from '@/shared/ui/redesigned/Icon';
 
 import cls from './Sidebar.module.scss';
 import { getSidebarItems } from '../../model/selectors/getSidebarItems';
@@ -25,6 +27,19 @@ export const Sidebar = memo((props: SidebarProps) => {
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
+
+    const itemsList = useMemo(
+        () =>
+            sidebarItemsList.map((item) => (
+                <SidebarItem
+                    item={item}
+                    collapsed={collapsed}
+                    key={item.path}
+                />
+            )),
+        [collapsed, sidebarItemsList],
+    );
+
     return (
         <ToggleFeatures
             feature="isAppRedesigned"
@@ -33,11 +48,28 @@ export const Sidebar = memo((props: SidebarProps) => {
                     data-testid="sidebar"
                     className={classNames(
                         cls.SidebarRedesigned,
-                        { [cls.collapsed]: collapsed },
+                        { [cls.collapsedRedesigned]: collapsed },
                         [className],
                     )}
                 >
-                    <AppLogo className={cls.appLogo} />
+                    <AppLogo
+                        size={collapsed ? 30 : 50}
+                        className={cls.appLogo}
+                    />
+                    <VStack role="navigation" gap="8" className={cls.items}>
+                        {itemsList}
+                    </VStack>
+                    <Icon
+                        data-testid="sidebar-toggle"
+                        onClick={onToggle}
+                        className={cls.collapseBtn}
+                        Svg={ArrowIcon}
+                        clickable
+                    />
+                    <div className={cls.switchers}>
+                        <ThemeSwitcher />
+                        <LangSwitcher short={collapsed} className={cls.lang} />
+                    </div>
                 </aside>
             }
             off={
@@ -45,9 +77,7 @@ export const Sidebar = memo((props: SidebarProps) => {
                     data-testid="sidebar"
                     className={classNames(
                         cls.Sidebar,
-                        {
-                            [cls.collapsed]: collapsed,
-                        },
+                        { [cls.collapsed]: collapsed },
                         [className],
                     )}
                 >
@@ -62,18 +92,12 @@ export const Sidebar = memo((props: SidebarProps) => {
                         {collapsed ? '>' : '<'}
                     </Button>
                     <VStack role="navigation" gap="8" className={cls.items}>
-                        {sidebarItemsList.map((item) => (
-                            <SidebarItem
-                                item={item}
-                                collapsed={collapsed}
-                                key={item.path}
-                            />
-                        ))}
+                        {itemsList}
                     </VStack>
 
                     <div className={cls.switchers}>
                         <ThemeSwitcher />
-                        <LangSwitcher className={cls.lang} short={collapsed} />
+                        <LangSwitcher short={collapsed} className={cls.lang} />
                     </div>
                 </aside>
             }
--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -1,10 +1,16 @@
-import React, { memo } from 'react';
+import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
 import { getUserAuthData } from '@/entities/User';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink';
+import { ToggleFeatures } from '@/shared/lib/features';
+import {
+    AppLink as AppLinkDeprecated,
+    AppLinkTheme,
+} from '@/shared/ui/deprecated/AppLink';
+import { AppLink } from '@/shared/ui/redesigned/AppLink';
+import { Icon } from '@/shared/ui/redesigned/Icon';
 
 import cls from './SidebarItem.module.scss';
 import { SidebarItemType } from '../../model/types/sidebar';
@@ -13,6 +19,7 @@ interface SidebarItemProps {
     item: SidebarItemType;
     collapsed: boolean;
 }
+
 export const SidebarItem = memo(({ item, collapsed }: SidebarItemProps) => {
     const { t } = useTranslation();
     const isAuth = useSelector(getUserAuthData);
@@ -20,14 +27,34 @@ export const SidebarItem = memo(({ item, collapsed }: SidebarItemProps) => {
     if (item.authOnly && !isAuth) {
         return null;
     }
+
     return (
-        <AppLink
-            to={item.path}
-            theme={AppLinkTheme.INVERTED}
-            className={classNames(cls.item, { [cls.collapsed]: collapsed })}
-        >
-            <item.Icon className={cls.icon} />
-            <span className={cls.link}>{t(item.text)}</span>
-        </AppLink>
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={
+                <AppLink
+                    to={item.path}
+                    className={classNames(cls.itemRedesigned, {
+                        [cls.collapsedRedesigned]: collapsed,
+                    })}
+                    activeClassName={cls.active}
+                >
+                    <Icon Svg={item.Icon} />
+                    <span className={cls.link}>{t(item.text)}</span>
+                </AppLink>
+            }
+            off={
+                <AppLinkDeprecated
+                    theme={AppLinkTheme.SECONDARY}
+                    to={item.path}
+                    className={classNames(cls.item, {
+                        [cls.collapsed]: collapsed,
+                    })}
+                >
+                    <item.Icon className={cls.icon} />
+                    <span className={cls.link}>{t(item.text)}</span>
+                </AppLinkDeprecated>
+            }
+        />
     );
 });
